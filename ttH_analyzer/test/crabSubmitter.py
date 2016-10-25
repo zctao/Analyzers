94103051ab63e6916c2bf5958a731c893988f95c
zctao <zt75@cornell.edu>
2016-10-24 21:36:26 -0500

    Update for 2016

--- ttH_analyzer/test/crabSubmitter.py
+++ ttH_analyzer/test/crabSubmitter.py
@@ -8,10 +8,10 @@ config = config()
 
 config.General.requestName = '%(name)s'
 config.General.transferLogs = True
-config.General.workArea = 'crab'
+config.General.workArea = '/uscms/home/ztao/nobackup/crab'
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ttH_2lss_tau_cfg.py'
+config.JobType.psetName = 'CU_ttH_EDA_cfg.py'
 config.JobType.pyCfgParams = %(cfgparams)s
 
 config.Data.inputDataset = %(dataset)s
@@ -27,20 +27,21 @@ config.Site.storageSite = 'T3_US_FNALLPC'
 '''
 
 channels = [#'ttH_htt', 'ttH_hww', 'ttH_hzz', 
-            #'TTW', 'TTZ',
+            'TTW', 'TTW_ext',
+            'TTZ',
             #'WZ',
             #'TTJets',
             #'rares_TTTT', 'rares_tZq', 'rares_WW', 'rares_WZZ', 
-            #'flips_data_dimu_2015c', 'flips_data_dimu_2015d',
-            #'flips_data_mu_2015c', 'flips_data_mu_2015d',
-            #'flips_data_mueg_2015c', 'flips_data_mueg_2015d',
-            #'flips_data_e_2015c', 'flips_data_e_2015d',
-            #'flips_data_dieg_2015c', 'flips_data_dieg_2015d',
-            #'fakes_data_dimu_2015c', 'fakes_data_dimu_2015d',
-            #'fakes_data_mu_2015c', 'fakes_data_mu_2015d',
-            #'fakes_data_mueg_2015c', 'fakes_data_mueg_2015d',
-            #'fakes_data_e_2015c', 'fakes_data_e_2015d',
-            #'fakes_data_dieg_2015c', 'fakes_data_dieg_2015d',
+            'flips_data_dimu_2016b','flips_data_dimu_2016c','flips_data_dimu_2016d',
+            'flips_data_mu_2016b','flips_data_mu_2016c','flips_data_mu_2016d',
+            'flips_data_mueg_2016b','flips_data_mueg_2016c','flips_data_mueg_2016d',
+            'flips_data_e_2016b','flips_data_e_2016c','flips_data_e_2016d',
+            'flips_data_dieg_2016b','flips_data_dieg_2016c','flips_data_dieg_2016d',
+            'fakes_data_dimu_2016b','fakes_data_dimu_2016c','fakes_data_dimu_2016d',
+            'fakes_data_mu_2016b','fakes_data_mu_2016c','fakes_data_mu_2016d',
+            'fakes_data_mueg_2016b','fakes_data_mueg_2016c','fakes_data_mueg_2016d',
+            'fakes_data_e_2016b','fakes_data_e_2016c','fakes_data_e_2016d',
+            'fakes_data_dieg_2016b','fakes_data_dieg_2016c','fakes_data_dieg_2016d',
             #'data_obs_dimu_2015c', 'data_obs_dimu_2015d',
             #'data_obs_mu_2015c', 'data_obs_mu_2015d',
             #'data_obs_mueg_2015c', 'data_obs_mueg_2015d',
@@ -49,7 +50,7 @@ channels = [#'ttH_htt', 'ttH_hww', 'ttH_hzz',
             ]
 
 for ch in channels:
-    with open("../data/SampleList.txt") as f:
+    with open("../data/SampleList_Sync.txt") as f:
         for line in f:
             if ch in line:
                 sample = f.next().strip()
@@ -64,7 +65,7 @@ for ch in channels:
     vd['cfgparams'] = pset
     vd['unit'] = perjob
     if 'data' in ch:
-        vd['lumimask'] = "config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'"
+        vd['lumimask'] = "config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Cert_271036-277933_13TeV_PromptReco_Collisions16_JSON.txt'"
         vd['runrange'] = 'config.Data.runRange = '+ run
         vd['splitting'] = 'LumiBased'
     else:
