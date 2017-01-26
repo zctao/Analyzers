4e9bbbca755f14f8a328c63134ae4c2238d1a970
zctao <zt75@cornell.edu>
2017-01-26 00:07:00 -0600

    Update Moriond17 sample list and corresponding cross sections, etc.

--- ttH_analyzer/macro/Cross_Sections.h
+++ ttH_analyzer/macro/Cross_Sections.h
@@ -5,12 +5,35 @@
 
 namespace xsection{  // unit: pb
 	std::map<std::string, float> xsection =
-		{{"ttH",0.2586},          // ttH, H->nonbb
-		 {"TTW",0.2043},          // TTWJetsToLNu
-		 {"TTZ",0.2529},          // TTZToLLNuNu
-		 {"TTJets_ll",87.3},      // TTJets_DiLept
-		 {"TTJets_lt",182},       // TTJets_SingleLeptFromT
-		 {"TTJets_ltbar",182}     // TTJets_SingleLeptFromTbar
+		{{"ttH",0.215},          // ttH, H->nonbb
+		 {"TTW",0.204},          // TTWJetsToLNu
+		 {"TTZ",0.253},          // TTZToLLNuNu
+		 {"TTGJets",3.70},       // TTGJets	 
+		 {"TGJets",2.97},        // TGJets
+		 {"WG",586},             // WGToLNuG
+		 {"ZG",131},             // ZGTo2LG
+		 {"WW",10.5},            // WWTo2L2Nu
+		 {"WpWp",0.0371},        // WpWpJJ
+		 {"WZ",4.10},            // WZTo3LNu
+		 {"ZZ",1.26},            // ZZTo4L
+		 {"WWW",0.209},          // WWW_4F
+		 {"WWZ",0.165},          // WWZ
+		 {"WZZ",0.0557},         // WZZ
+		 {"ZZZ",0.014},          // ZZZ
+		 {"tZq",0.0758},         // tZq_ll_4f
+		 {"TTTT",0.0091},        // TTTT
+		 {"TTJets_ll",87.3},     // TTJets_DiLept
+		 {"TTJets_lt",182},      // TTJets_SingleLeptFromT
+		 {"TTJets_ltbar",182},   // TTJets_SingleLeptFromTbar
+		 {"DYJets_M10to50",18600},// DYJetsToLL_M-10to50
+		 {"DYJets_M50",5770},    // DYJetsToLL_M-50
+		 {"WJets",61500},        // WJetsToLNu
+		 {"ST_sLep",3.75},       // ST_s-channel_4f_lep
+		 {"ST_tT",70.7},         // ST_t-channel_top_4f
+		 {"ST_tTbar",70.7},      // ST_t-channel_antitop_4f
+		 {"ST_tWT",35.6},        // ST_tW_top_5f
+		 {"ST_tWTbar",35.6},     // ST_tW_antitop_5f
+		 {"WWds",} // WW_DoubleScattering
 		};
 }
 
--- ttH_analyzer/macro/Misc_Constants.h
+++ ttH_analyzer/macro/Misc_Constants.h
@@ -46,22 +46,44 @@ const std::map<TString,std::vector<TString>> SamplesInChannel =
 
 /////////////////////////////////////////////
 // Crab job output directories for different samples
+TString batch = "jan2017/";
+TString eos_dir = "/eos/uscms/store/user/ztao/ttH_80X/"
 const std::map<TString, TString> dir_map =
-	{{"ttH", "/eos/uscms/store/user/ztao/ttH_80X/ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/"},
-	 {"TTZ", "/eos/uscms/store/user/ztao/ttH_80X/TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8/"},
-	 {"TTW", "/eos/uscms/store/user/ztao/ttH_80X/TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/"},
-	 {"WZ", "/eos/uscms/store/user/ztao/ttH_80X/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/"},
-	 {"ZZ", "/eos/uscms/store/user/ztao/ttH_80X/ZZTo4L_13TeV-amcatnloFXFX-pythia8/"},
-	 {"WW", "/eos/uscms/store/user/ztao/ttH_80X/WWTo2L2Nu_13TeV-powheg/"},
-	 {"WG", "/eos/uscms/store/user/ztao/ttH_80X/WGToLNuG_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/"},
-	 {"ZG", "/eos/uscms/store/user/ztao/ttH_80X/ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/"},
-	 {"TTTT", "/eos/uscms/store/user/ztao/ttH_80X/TTTT_TuneCUETP8M1_13TeV-amcatnlo-pythia8/"},
-	 //{"tZq", "/"},
-	 {"DoubleMuon", "/eos/uscms/store/user/ztao/ttH_80X/DoubleMuon/"},
-	 {"SingleMuon", "/eos/uscms/store/user/ztao/ttH_80X/SingleMuon/"},
-	 {"DoubleEG", "/eos/uscms/store/user/ztao/ttH_80X/DoubleEG/"},
-	 {"SingleElectron", "/eos/uscms/store/user/ztao/ttH_80X/SingleElectron/"},
-	 {"MuonEG", "/eos/uscms/store/user/ztao/ttH_80X/MuonEG/"}
+	{{"ttH", eos_dir+"ttHToNonbb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/"+batch},
+	 {"TTW", eos_dir+"TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/"+batch},
+	 {"TTZ", eos_dir+"TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8/"+batch},
+	 {"TTGJets", eos_dir+"TTGJets_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/"+batch},
+	 {"TGJets", eos_dir+"TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8/"+batch},
+	 {"WG", eos_dir+"WGToLNuG_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/"+batch},
+	 {"ZG", eos_dir+"ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/"+batch},
+	 {"WW", eos_dir+"WWTo2L2Nu_13TeV-powheg/"+batch},
+	 {"WpWp", eos_dir+"WpWpJJ_EWK-QCD_TuneCUETP8M1_13TeV-madgraph-pythia8/"+batch},
+	 {"WZ", eos_dir+"WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/"+batch},
+	 {"ZZ", eos_dir+"ZZTo4L_13TeV-amcatnloFXFX-pythia8/"+batch},
+	 {"WWW", eos_dir+""+batch},
+	 {"WWZ", eos_dir+""+batch},
+	 {"WZZ", eos_dir+""+batch},
+	 {"ZZZ", eos_dir+""+batch},
+	 {"tZq", eos_dir+"tZq_ll_4f_13TeV-amcatnlo-pythia8/"+batch},
+	 {"TTTT", eos_dir+"TTTT_TuneCUETP8M1_13TeV-amcatnlo-pythia8/"+batch},
+	 {"TTJets_ll",},
+	 {"TTJets_lt",},
+	 {"TTJets_ltbar",},
+	 {"DYJets_M10to50", eos_dir+"DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/"+batch},
+	 {"DYJets_M50", eos_dir+"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/"+batch},
+	 {"WJets",eos_dir+"WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/"+batch},
+	 {"ST_sLep",},
+	 {"ST_tT",},
+	 {"ST_tTbar",},
+	 {"ST_tWT",},
+	 {"ST_tWTbar",},
+	 {"WWds", eos_dir+""+batch},
+	 
+	 {"DoubleMuon", "/eos/uscms/store/user/ztao/ttH_80X/DoubleMuon/"+batch},
+	 {"SingleMuon", "/eos/uscms/store/user/ztao/ttH_80X/SingleMuon/"+batch},
+	 {"DoubleEG", "/eos/uscms/store/user/ztao/ttH_80X/DoubleEG/"+batch},
+	 {"SingleElectron", "/eos/uscms/store/user/ztao/ttH_80X/SingleElectron/"+batch},
+	 {"MuonEG", "/eos/uscms/store/user/ztao/ttH_80X/MuonEG/"+batch}
 	};
 
 #endif
