e1c5bb8643761b264a5f0f23629bc0a6d9b03dc3
zctao <zt75@cornell.edu>
2017-02-15 15:41:52 -0600

    Update produceSyncNtuples script

--- ttH_analyzer/test/produceSyncNtuples.sh
+++ ttH_analyzer/test/produceSyncNtuples.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # sync ntuple for signal region
 echo 'start producing sync ntuple for signal region ...'
-cmsRun CU_ttH_EDA_cfg.py evtSelOff=False TurnOffHLTCut=True SampleName=sync_event_sr doSystematics=True SelectionRegion=signal_2lss1tau reHLT=False GridMode=False doJERSmearing=False
+cmsRun CU_ttH_EDA_cfg.py evtSelOff=False TurnOffHLTCut=True SampleName=sync_event_sr doSystematics=True SelectionRegion=signal_2lss1tau GridMode=False
 
 # sync ntuple for fake extrapolation region
 echo 'start producing sync ntuple for fake extrapolation region ...'
-cmsRun CU_ttH_EDA_cfg.py evtSelOff=False TurnOffHLTCut=True SampleName=sync_event_fake doSystematics=False SelectionRegion=control_1lfakeable reHLT=False GridMode=False doJERSmearing=False
+cmsRun CU_ttH_EDA_cfg.py evtSelOff=False TurnOffHLTCut=True SampleName=sync_event_fake doSystematics=False SelectionRegion=control_1lfakeable GridMode=False
 
 # sync ntuple for charge flip control region
 echo 'start producing sync ntuple for charge flip control region ...'
-cmsRun CU_ttH_EDA_cfg.py evtSelOff=False TurnOffHLTCut=True SampleName=sync_event_flip doSystematics=False SelectionRegion=control_2los1tau reHLT=False GridMode=False doJERSmearing=False
+cmsRun CU_ttH_EDA_cfg.py evtSelOff=False TurnOffHLTCut=True SampleName=sync_event_flip doSystematics=False SelectionRegion=control_2los1tau GridMode=False
