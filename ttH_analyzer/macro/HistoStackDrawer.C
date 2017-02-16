add1582db9a372ec5667b041f918b59381f57e31
Zhengcheng Tao <zt75@cornell.edu>
2017-02-16 16:12:06 -0500

    Better plotting

--- ttH_analyzer/macro/HistoStackDrawer.C
+++ ttH_analyzer/macro/HistoStackDrawer.C
@@ -7,10 +7,8 @@ void HistoStackDrawer(std::map<TString, TH1D*> histoMap)
 		return;
 	}
 
-	TCanvas c;
-
 	THStack* hs = new THStack("", "");
-	TLegend* l = new TLegend(0.78,0.60,0.95,0.95);
+	TLegend* l = new TLegend(0.82,0.30,0.95,0.57);
 
 	TH1D* h_obs = histoMap["data_obs"];
 	SetHistStyle(h_obs, "data_obs");
@@ -34,7 +32,7 @@ void HistoStackDrawer(std::map<TString, TH1D*> histoMap)
 			channel = "Fakes";
 		if (channel=="TTZ")
 			channel = "ttZ";
-		if (channel=="TTZ")
+		if (channel=="TTW")
 			channel = "ttW";
 		
 		SetHistStyle(hm.second, channel);
@@ -42,21 +40,151 @@ void HistoStackDrawer(std::map<TString, TH1D*> histoMap)
 		hs->Add(hm.second);
 		l->AddEntry(hm.second, channel, "f");
 	}
-
+	
 	// plotting
-	TString pname = hs->GetName();
+	//gROOT->LoadMacro("tdrstyle.C");
+	setTDRStyle();
 
+	// CMS_lumi
+	//gROOT->LoadMacro("CMS_lumi.C");
+	writeExtraText = true;
+	extraText = "Preliminary";
+	lumi_13TeV = "36.8 fb^{-1}";
+	lumi_sqrtS = "13 TeV";
+
+	int iPeriod = 4; // 4=13TeV, 0=free form (use lumi_sqrtS)
+	int iPos = 0;
+	if (iPos==0) relPosX = 0.12;
+
+	// Canvas
+	TCanvas* c = new TCanvas("c","",0,0,600,600);
+	
+	// for showing ratio and uncertainties
 	float ymax = max(hs->GetMaximum(),h_obs->GetMaximum());
-	hs->SetMaximum(ymax*1.2);
-	
-	hs->Draw("HIST");
-	hs->GetXaxis()->SetTitle(pname);
-	gPad->Modified();
-	
-	h_obs->Draw("same");
-	l->Draw("same");
 
-	c.SaveAs(pname+".pdf");
+	// upper pad
+	TPad* pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
+	pad1->SetTopMargin(0.08);
+	pad1->SetBottomMargin(0.); // Upper and lower plot are joined
+	//pad1->SetGridx();         // Vertical grid
+	pad1->Draw();             // Draw the upper pad: pad1	
+	pad1->cd();
+
+	hs->SetMaximum(ymax*1.2);
+	hs->Draw("HIST");
+	l->Draw("same");
+	h_obs->Draw("same");	
+	
+	c->cd();
+
+	// lower pad
+	TPad* pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
+	pad2->SetTopMargin(0);
+	pad2->SetBottomMargin(0.2);
+	pad2->Draw();
+	pad2->cd();
+
+	// define the ratio plot
+	TH1D* h_tot = (TH1D*)(hs->GetStack()->Last())->Clone();
+	TH1D* h_ratio = (TH1D*)h_obs->Clone();
+	TH1D* h_err = (TH1D*)h_tot->Clone();
+	h_ratio->Divide(h_tot);
+	h_err->Divide(h_tot);
+	
+	h_ratio->SetStats(0);
+	h_ratio->Draw("ep");
+	
+	h_err->SetLineColor(1);
+	h_err->SetFillColor(1);
+	h_err->SetFillStyle(3003);
+	h_err->Draw("same E2");
+
+	float xmax = h_err->GetXaxis()->GetXmax();
+	float xmin = h_err->GetXaxis()->GetXmin();
+	TF1 *f1 = new TF1("f1","1",xmin,xmax);
+	f1->SetLineColor(1);
+	f1->SetLineWidth(1);
+	f1->SetLineStyle(1);
+	f1->Draw("same");
+
+	// Upper pad Y axis
+	pad1->cd();
+	hs->GetYaxis()->SetTitle("Events");
+	hs->GetYaxis()->SetTitleOffset(1.25);
+	hs->GetYaxis()->SetTitleSize();
+	hs->GetYaxis()->SetNdivisions(510);
+	hs->GetYaxis()->SetLabelSize(0.035);
+	hs->GetYaxis()->ChangeLabel(1,0,0);
+	
+	// Lower pad Y axis
+	pad2->cd();
+	h_ratio->GetYaxis()->SetTitle("Data/Pred.");
+	h_ratio->GetYaxis()->SetTitleOffset(0.51);
+	h_ratio->GetYaxis()->SetTitleSize(0.1);
+	h_ratio->GetYaxis()->CenterTitle();
+	h_ratio->GetYaxis()->SetNdivisions(505);
+	h_ratio->GetYaxis()->SetRangeUser(0.0,2.0);
+	h_ratio->GetYaxis()->SetLabelSize(0.10);
+	h_ratio->GetYaxis()->ChangeLabel(1,0,0);
+	h_ratio->GetYaxis()->ChangeLabel(-1,0,0);
+	// Lower pad X axis
+	TString pname = hs->GetName();
+	h_ratio->GetXaxis()->SetTitle(properTitle[pname]);
+	h_ratio->GetXaxis()->SetTitleSize(0.1);
+	//h_ratio->GetXaxis()->SetTitleOffset(4.0);
+	h_ratio->GetXaxis()->SetNdivisions(510);
+	h_ratio->GetXaxis()->SetLabelSize(0.1);
+	
+	// writing the lumi information and the CMS "logo"
+	CMS_lumi( c, iPeriod, iPos );
+
+	c->Update();
+	c->RedrawAxis();
+	c->GetFrame()->Draw();
+	
+	c->SaveAs(pname+".pdf");
+	delete c;
+}
+
+void HistoStackDrawer(TString filename="histograms.root")
+{
+	TFile* f = TFile::Open(filename);
+
+	TIter next(f->GetListOfKeys());
+	TKey *key;
+
+	std::vector<TString> channels;
+	
+	while ((key = (TKey*)next())) {
+		channels.push_back(key->GetTitle());
+	}
+
+
+	std::map<TString, std::vector<TH1D*>> histoCollection;
+
+	for (auto & ch : channels) {
+
+		//std::cout << ch << std::endl;
+		std::vector<TH1D*> hists;
+		
+		f->cd(ch);
+		TIter subnext(gDirectory->GetListOfKeys());
+		TKey *subkey;
+
+		while ((subkey = (TKey*)subnext())) {
+			TClass *c1 = gROOT->GetClass(subkey->GetClassName());
+			if (!c1->InheritsFrom("TH1")) continue;
+
+			TH1D* h = (TH1D*)subkey->ReadObj();
+			hists.push_back(h);
+			//std::cout << h->GetName() << std::endl;
+		}
+
+		histoCollection[ch] = hists;
+	}
+
+	HistoStackDrawer(histoCollection);	
+	
 }
 
 void HistoStackDrawer(std::vector<TString> channels, std::vector<TH1D*> hists)
@@ -91,46 +219,5 @@ void HistoStackDrawer(std::map<TString, std::vector<TH1D*>> histoCollection)
 
 		HistoStackDrawer(histoMap);
 	}
-}
-
-void HistoStackDrawer(TString filename)
-{
-	TFile* f = TFile::Open(filename);
-
-	TIter next(f->GetListOfKeys());
-	TKey *key;
-
-	std::vector<TString> channels;
-	
-	while ((key = (TKey*)next())) {
-		channels.push_back(key->GetTitle());
-	}
-
-
-	std::map<TString, std::vector<TH1D*>> histoCollection;
-
-	for (auto & ch : channels) {
-
-		std::cout << ch << std::endl;
-		std::vector<TH1D*> hists;
-		
-		f->cd(ch);
-		TIter subnext(gDirectory->GetListOfKeys());
-		TKey *subkey;
-
-		while ((subkey = (TKey*)subnext())) {
-			TClass *c1 = gROOT->GetClass(subkey->GetClassName());
-			if (!c1->InheritsFrom("TH1")) continue;
-
-			TH1D* h = (TH1D*)subkey->ReadObj();
-			hists.push_back(h);
-			std::cout << h->GetName() << std::endl;
-		}
-
-		histoCollection[ch] = hists;
-	}
-
-	HistoStackDrawer(histoCollection);	
-	
 }
 
--- ttH_analyzer/macro/HistoStackDrawer.h
+++ ttH_analyzer/macro/HistoStackDrawer.h
@@ -5,6 +5,7 @@
 #include "TFile.h"
 #include "TH1.h"
 #include "TH1D.h"
+#include "TF1.h"
 #include "THStack.h"
 #include "TLegend.h"
 #include "TString.h"
@@ -13,6 +14,10 @@
 #include "TKey.h"
 #include "TClass.h"
 #include "TCanvas.h"
+#include "TPad.h"
+
+#include "tdrstyle.C"
+#include "CMS_lumi.C"
 
 #include <iostream>
 #include <vector>
@@ -26,26 +31,32 @@ void SetHistStyle(TH1D* h, TString channel)
 	}
 	else if (channel=="ttH") {
 		h->SetFillColor(kRed);
+		h->SetLineColor(kBlack);
 		//h->SetFillStyle();
 	}
 	else if (channel=="TTW" or channel=="ttW") {
 		h->SetFillColor(kGreen+2);
+		h->SetLineColor(kBlack);
 		//h->SetFillStyle();
 	}
 	else if (channel=="TTZ" or channel=="ttZ") {
 		h->SetFillColor(kGreen-8);
+		h->SetLineColor(kBlack);
 		//h->SetFillStyle();
 	}
 	else if (channel=="EWK" or channel=="Electroweak") {
 		h->SetFillColor(kViolet-2);
+		h->SetLineColor(kBlack);
 		//h->SetFillStyle();
 	}
 	else if (channel=="Rares") {
 		h->SetFillColor(kAzure+1);
+		h->SetLineColor(kBlack);
 		//h->SetFillStyle();
 	}
 	else if (channel=="Fakes" or channel=="fakes_data") {
 		h->SetFillColor(kBlack);
+		h->SetLineColor(kBlack);
 		h->SetFillStyle(3335);
 	}
 	else {
@@ -59,5 +70,24 @@ void HistoStackDrawer(std::map<TString, TH1D*>);
 void HistoStackDrawer(std::map<TString, std::vector<TH1D*>>);
 void HistoStackDrawer(std::vector<TString>, std::vector<TH1D*>);
 void HistoStackDrawer(TString);
+
+std::map<TString, TString> properTitle = {
+	{"tau_pt","#tau_{h} p_{T} [GeV]"},
+	{"njet", "N(jet, p_{T}>25 GeV)"},
+	{"mindr_lep1_jet","mindr_lep1_jet"},
+	{"mindr_lep2_jet","mindr_lep2_jet"},
+	{"avg_dr_jet","avg_dr_jet"},
+	{"max_lep_eta","max(#eta_{l_{1}},#eta_{l_{2}})"},
+	{"met","E^{miss}_{T} [GeV]"},
+	{"mT_met_lep1","mT_met_lep1"},
+	{"mht","MHT [GeV]"},
+	{"dr_leps","dR(l_{1},l_{2})"},
+	{"dr_lep1_tau","dR(l_{ldg},#tau)"},
+	{"lep1_pt","Leading lepton p_{T} [GeV]"},
+	{"lep2_pt","Subleading lepton p_{T} [GeV]"},
+	{"mTauTauVis1","m^{vis}_{#tau l_{1}}"},
+	{"mTauTauVis2","m^{vis}_{#tau l_{2}}"},
+	{"sum_charges","Sum of charge"}
+};
 
 #endif
