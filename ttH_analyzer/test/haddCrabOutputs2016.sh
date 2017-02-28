4a24de6ae054bddb57776bc6d61201266d806265
zctao <zt75@cornell.edu>
2017-02-27 21:57:05 -0600

    Update hadd script and constants header file for 2016 re-miniAOD dataset

--- ttH_analyzer/test/haddCrabOutputs2016.sh
+++ ttH_analyzer/test/haddCrabOutputs2016.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "hadding dataset: DoubleEG"
-eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/DoubleEG/ [b,c,d,e,f,g,h_v2,h_v3] feb2017 170204
+eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/DoubleEG/ [b,c,d,e,f,g,h_v2,h_v3] 24feb2017 170224
 
 echo "hadding dataset: DoubleMuon"
-eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/DoubleMuon/ [b,c,d,e,f,g,h_v2,h_v3] feb2017 170204
+eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/DoubleMuon/ [b,c,d,e,f,g,h_v2,h_v3] 24feb2017 170224
 
 echo "hadding dataset: MuonEG"
-eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/MuonEG/ [b,c,d,e,f,g,h_v2,h_v3] feb2017 170204
+eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/MuonEG/ [b,c,d,e,f,g,h_v2,h_v3] 24feb2017 170224
 
 echo "hadding dataset: SingleElectron"
-eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/SingleElectron/ [b,c,d,e,f,g,h_v2,h_v3] feb2017 170204
+eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/SingleElectron/ [b,c,d,e,f,g,h_v2,h_v3] 24feb2017 170224
 
 echo "hadding dataset: SingleMuon"
-eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/SingleMuon/ [b,c,d,e,f,g,h_v2,h_v3] feb2017 170204
+eval ./haddDataCrabOutputs2016.sh /eos/uscms/store/user/ztao/ttH_80X/SingleMuon/ [b,c,d,e,f,g,h_v2,h_v3] 24feb2017 170224
